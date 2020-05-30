abc95f55b9ce91769bb7c5cae4c007d671c398b2
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-05-30 20:12:02 +0200

    feat: update signin mutation

--- client/src/graphql/mutations/signIn.mutation.ts
+++ client/src/graphql/mutations/signIn.mutation.ts
@@ -1,8 +1,8 @@
 import gql from 'graphql-tag';
 
 export default gql`
-  mutation signIn($username: String!, $password: String!) {
-    signIn(username: $username, password: $password) {
+  mutation signIn($email: String!, $password: String!) {
+    signIn(email: $email, password: $password) {
       id
     }
   }
