70ca50cc124c07a1400281accea879cd6e8faf23
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-05-30 21:55:09 +0200

    update theme and add form-styles

--- client/src/components/AuthScreen/SignInForm.tsx
+++ client/src/components/AuthScreen/SignInForm.tsx
@@ -10,6 +10,20 @@ import {
   Button,
   ErrorMessage,
 } from './form-components';
+
+import {
+  LoginFormContainer,
+  Label,
+  Input,
+  InputContainer,
+  StyledButton,
+  LoginHeading,
+  ErrorMessageContainer,
+  ErrorMessageHeading,
+  RegisterContainer,
+  RegisterLink,
+} from './form-styles';
+
 import { RouteComponentProps } from 'react-router-dom';
 
 const SignInForm: React.FC<RouteComponentProps<any>> = ({ history }) => {
@@ -78,6 +92,74 @@ const SignInForm: React.FC<RouteComponentProps<any>> = ({ history }) => {
       </ActualForm>
     </SignForm>
   );
+  // return (
+  //   <LoginFormContainer>
+  //     <LoginHeading>Sign in to Fakebooker</LoginHeading>
+  //     <form onSubmit={handleSubmit(onSubmit)}>
+  //       <InputContainer>
+  //         <Label>Email</Label>
+  //         <Input
+  //           name="email"
+  //           ref={register({
+  //             required: 'Email is required',
+  //           })}
+  //         />
+  //         {errors && errors.email && (
+  //           <ErrorMessageContainer>
+  //             <ErrorIcon width={20} height={20} fill="#d93025" />
+  //             <ErrorMessageHeading>{errors.email.message}</ErrorMessageHeading>
+  //           </ErrorMessageContainer>
+  //         )}
+  //       </InputContainer>
+  //       <InputContainer>
+  //         <Label>Password</Label>
+  //         <Input
+  //           name="password"
+  //           type="password"
+  //           ref={register({
+  //             required: 'Password is required',
+  //           })}
+  //         />
+  //         {errors && errors.password && (
+  //           <ErrorMessageContainer>
+  //             <ErrorIcon width={20} height={20} fill="#d93025" />
+  //             <ErrorMessageHeading>
+  //               {errors.password.message}
+  //             </ErrorMessageHeading>
+  //           </ErrorMessageContainer>
+  //         )}
+  //       </InputContainer>
+  //       <StyledButton htmlType="submit" disabled={loading}>
+  //         Sign in
+  //         {loading && (
+  //           <Loader
+  //             type="TailSpin"
+  //             color="#fff"
+  //             style={{
+  //               position: 'absolute',
+  //               right: '16px',
+  //               top: '12px',
+  //             }}
+  //             height={20}
+  //             width={20}
+  //           />
+  //         )}
+  //       </StyledButton>
+  //       {graphQLError && (
+  //         <ErrorMessageContainer>
+  //           <ErrorIcon width={20} height={20} fill="#d93025" />
+  //           <ErrorMessageHeading>{graphQLError.message}</ErrorMessageHeading>
+  //         </ErrorMessageContainer>
+  //       )}
+  //     </form>
+  //     <RegisterContainer>
+  //       Not a member?
+  //       <Link to="/register">
+  //         <RegisterLink>Sign up now</RegisterLink>
+  //       </Link>
+  //     </RegisterContainer>
+  //   </LoginFormContainer>
+  // );
 };
 
 export default SignInForm;
--- client/src/styles/theme.tsx
+++ client/src/styles/theme.tsx
@@ -1,5 +1,14 @@
 export const theme = {
-  colors: {
-    white: '#ffffff',
-  },
+  primaryText: '#1876f2',
+  secondaryText: '#050505',
+  tertiaryText: '#65676b',
+  errorText: '#D93025',
+  inputColor: '#f0f2f5',
+  placeholderColor: '#90949c',
+  primaryBackground: '#e7f3ff',
+  secondaryBackground: '#e4e6eb',
+  secondaryHoverBackground: '#d8dadf',
+  tertiaryBackground: '#F2F2F2',
+  boxShadow1: '0 1px 2px 0 rgba(0, 0, 0, 0.1)',
+  boxShadow2: '0 1px 2px 0 rgba(0, 0, 0, 0.2)',
 };
