3201de93147be776ed129524e69484f8dcc839ec
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-06-02 20:15:15 +0200

    arefactor: remove router animation not needed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,27 +9,25 @@ import AuthScreen from './components/AuthScreen';
 import ChatRoomScreen from './components/ChatRoomScreen';
 import ChatsListScreen from './components/ChatsListScreen';
 import ChatCreationScreen from './components/ChatCreationScreen';
-import AnimatedSwitch from './components/AnimatedSwitch';
+
 import { withAuth } from './services/auth.service';
 
 const App: React.FC = () => (
   <BrowserRouter>
-    <AnimatedSwitch>
-      <Route exact path="/sign-(in|up)" component={AuthScreen} />
-      <Route exact path="/chats" component={withAuth(ChatsListScreen)} />
+    <Route exact path="/sign-(in|up)" component={AuthScreen} />
+    <Route exact path="/chats" component={withAuth(ChatsListScreen)} />
 
-      <Route
-        exact
-        path="/chats/:chatId"
-        component={withAuth(
-          ({ match, history }: RouteComponentProps<{ chatId: string }>) => (
-            <ChatRoomScreen chatId={match.params.chatId} history={history} />
-          )
-        )}
-      />
+    <Route
+      exact
+      path="/chats/:chatId"
+      component={withAuth(
+        ({ match, history }: RouteComponentProps<{ chatId: string }>) => (
+          <ChatRoomScreen chatId={match.params.chatId} history={history} />
+        )
+      )}
+    />
 
-      <Route exact path="/new-chat" component={withAuth(ChatCreationScreen)} />
-    </AnimatedSwitch>
+    <Route exact path="/new-chat" component={withAuth(ChatCreationScreen)} />
     <Route exact path="/" render={redirectToChats} />
   </BrowserRouter>
 );
