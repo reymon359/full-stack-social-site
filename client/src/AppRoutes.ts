996ab801eaedef7e096b10a1415b63f2254dcd97
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-06-03 08:58:37 +0200

    feat(routing): add app routes file

---
+++ client/src/AppRoutes.ts
@@ -0,0 +1,10 @@
+export const AppRoutes = {
+  Root: '/',
+  Home: '/home',
+  Auth: '/sign-(in|up)',
+  Profile: '/:username',
+  Post: '/post/:postId',
+  NewPost: '/new-post',
+  About: '/about',
+  All: '*',
+};
