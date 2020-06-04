f0c88be5f8b4dba4988176c61b9979757d6d4c66
reymon359 <ramonmorcillocascales@gmail.com>
2020-06-04 07:59:04 +0200

    feat: add navbar to pages missing it

--- client/src/pages/NotfoundPage/index.tsx
+++ client/src/pages/NotfoundPage/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { History } from 'history';
+import Navbar from '../../components/Navbar';
 
 interface NotfoundPageProps {
   history: History;
 }
 
 const NotfoundPage: React.FC<NotfoundPageProps> = ({ history }) => (
-  <h1>Notfound</h1>
+  <>
+    <Navbar history={history} />
+    <h1>404</h1>
+  </>
 );
 
 export default NotfoundPage;
