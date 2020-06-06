790bd02df5052ebf9e9f3d14f93bf60e709644b6
reymon359 <ramonmorcillocascales@gmail.com>
2020-06-06 23:04:47 +0200

    feat: display user image

--- client/src/components/ProfileDetails.tsx
+++ client/src/components/ProfileDetails.tsx
@@ -6,12 +6,21 @@ const ProfileDetailsContainer = styled.div`
   background-color: red;
 `;
 
+const UserPicture = styled.img`
+  border-radius: 100%;
+`;
+
 interface ProfileDetailsProps {
   user: User;
 }
 
 const ProfileDetails: React.FC<ProfileDetailsProps> = ({ user }): any => {
-  return <ProfileDetailsContainer>{user.username}</ProfileDetailsContainer>;
+  return (
+    <ProfileDetailsContainer>
+      <UserPicture src={user.picture} />
+      {user.username}
+    </ProfileDetailsContainer>
+  );
 };
 
 export default ProfileDetails;
