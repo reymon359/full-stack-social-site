8b234e1235b152af41c4438d557142147ed296d9
reymon359 <ramonmorcillocascales@gmail.com>
2020-06-08 07:43:49 +0200

    style: style user image

--- client/src/components/ProfileDetails.tsx
+++ client/src/components/ProfileDetails.tsx
@@ -3,11 +3,21 @@ import { User } from '../graphql/types';
 import styled from 'styled-components';
 
 const ProfileDetailsContainer = styled.div`
-  background-color: red;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0px auto;
+  padding: 2rem;
 `;
 
 const UserPicture = styled.img`
   border-radius: 100%;
+  width: 20%;
+  height: 20%;
+  min-height: 100px;
+  min-width: 100px;
+  border: 2px solid ${(props) => props.theme.colors.primary};
 `;
 
 interface ProfileDetailsProps {
