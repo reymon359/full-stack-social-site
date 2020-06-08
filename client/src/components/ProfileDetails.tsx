cb5d41ebcc161593e0ce01dd6ee81414de3cdd57
reymon359 <ramonmorcillocascales@gmail.com>
2020-06-08 08:44:04 +0200

    style: add follow styles

--- client/src/components/ProfileDetails.tsx
+++ client/src/components/ProfileDetails.tsx
@@ -8,7 +8,7 @@ const ProfileDetailsContainer = styled.div`
   justify-content: center;
   align-items: center;
   margin: 0px auto;
-  padding: 2rem;
+  padding: 2rem 1rem;
 `;
 
 const UserPicture = styled.img`
@@ -17,11 +17,11 @@ const UserPicture = styled.img`
   height: 20%;
   min-height: 100px;
   min-width: 100px;
-  border: 2px solid ${(props) => props.theme.colors.primary};
+  border: 3px solid ${(props) => props.theme.colors.primaryDark};
 `;
 
 const UserName = styled.h1`
-  padding: 0.5rem 0;
+  padding: 0.3rem 0;
   font-size: ${(props) => props.theme.fontSizes.xLarge};
   font-weight: ${(props) => props.theme.fontWeights.bold};
 
@@ -31,23 +31,49 @@ const UserName = styled.h1`
 `;
 
 const UserUsername = styled.h3`
-  padding: 0.2rem 0;
+  padding: 0.3rem 0;
+  color: ${(props) => props.theme.colors.dark};
   font-size: ${(props) => props.theme.fontSizes.medium};
-  font-weight: ${(props) => props.theme.fontWeights.regular};
+  font-weight: ${(props) => props.theme.fontWeights.bold};
 
   ${(props) => props.theme.media.sm`
-    font-size: ${props.theme.fontSizes.normal};
+    font-size: ${props.theme.fontSizes.medium};
  `}
 `;
 
 const UserBio = styled.p`
-  padding: 0.2rem 0;
+  padding: 0.3rem 0;
   font-size: ${(props) => props.theme.fontSizes.medium};
   font-weight: ${(props) => props.theme.fontWeights.regular};
+  width: 60%;
+  text-align: center;
 
   ${(props) => props.theme.media.sm`
     font-size: ${props.theme.fontSizes.normal};
+    width: 80%;
+
  `}
+`;
+
+const FollowsContainer = styled.div`
+  padding: 0.3rem 0;
+  font-size: ${(props) => props.theme.fontSizes.medium};
+  font-weight: ${(props) => props.theme.fontWeights.regular};
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  ${(props) => props.theme.media.sm`
+    font-size: ${props.theme.fontSizes.normal};
+ `}
+
+  p {
+    margin: 0 0.2rem;
+    font-weight: ${(props) => props.theme.fontWeights.bold};
+  }
+  a {
+    margin: 0 0.2rem;
+    // color: ${(props) => props.theme.colors.dark}; 
+  }
 `;
 
 interface ProfileDetailsProps {
@@ -60,8 +86,14 @@ const ProfileDetails: React.FC<ProfileDetailsProps> = ({ user }): any => {
     <ProfileDetailsContainer>
       <UserPicture src={user.picture} />
       <UserName> {user.name}</UserName>
-      <UserUsername>{user.username}</UserUsername>
+      <UserUsername>@{user.username}</UserUsername>
       <UserBio>{user.bio}</UserBio>
+      <FollowsContainer>
+        <p>{user.followers}</p>
+        <a href="#">Followers</a>
+        <p>{user.following}</p>
+        <a href="#">Following</a>
+      </FollowsContainer>
     </ProfileDetailsContainer>
   );
 };
