77827d709cc10c4e74a678ead1fdc2f0c39b6233
reymon359 <ramonmorcillocascales@gmail.com>
2020-06-11 22:15:35 +0200

    feat: finish post provider

--- server/modules/posts/posts.provider.ts
+++ server/modules/posts/posts.provider.ts
@@ -1,14 +1,6 @@
 import { Injectable, Inject, ProviderScope } from '@graphql-modules/di';
-import { QueryResult } from 'pg';
 import sql from 'sql-template-strings';
-import DataLoader from 'dataloader';
 import { Database } from '../common/database.provider';
-import { Post } from '../../db';
-
-type PostsByUser = { userId: string };
-type PostsLikedByUser = { userId: string };
-type PostById = { postId: string };
-type PostsKey = PostById | PostsByUser | PostsLikedByUser;
 
 @Injectable({
   scope: ProviderScope.Session,
@@ -16,84 +8,41 @@ type PostsKey = PostById | PostsByUser | PostsLikedByUser;
 export class Posts {
   @Inject() private db: Database;
 
-  private postsCache = new Map<string, Post>();
-  private loaders = {
-    posts: new DataLoader<PostsKey, QueryResult['rows']>((keys) => {
-      return Promise.all(
-        keys.map(async (query) => {
-          if (isPostByUser(query)) {
-            return this._findPostByUser(query.userId);
-          }
+  async lastPosts() {
+    const { rows } = await this.db.query(sql`
+      SELECT * FROM posts 
+      ORDER BY created_at DESC 
+      LIMIT 50
+    `);
 
-          if (isPostsByUser(query)) {
-            return this._findPostsByUser(query.userId);
-          }
+    return rows[0] || null;
+  }
 
-          if (this.postsCache.has(query.postId)) {
-            return [this._readPostFromCache(query.postId)];
-          }
+  async findPostById(postId: string) {
+    const { rows } = await this.db.query(sql`
+      SELECT * FROM posts WHERE id = ${postId}
+    `);
 
-          return this._findPostById(query.postId);
-        })
-      );
-    }),
-  };
+    return rows[0] || null;
+  }
 
   async findPostsByUser(userId: string) {
-    return this.loaders.posts.load({ userId });
-  }
-
-  private async _findPostsByUser(userId: string) {
     const { rows } = await this.db.query(sql`
-      SELECT posts.* FROM posts, posts_users
-      WHERE posts.id = posts_users.post_id
-      AND posts_users.user_id = ${userId}
-    `);
-
-    rows.forEach((row) => {
-      this._writePostToCache(row);
-    });
-
-    return rows;
-  }
-
-  async findPostByUser({ userId }: { userId: string }) {
-    const rows = await this.loaders.posts.load({ userId });
-    return rows[0] || null;
-  }
-  private async _findPostByUser({ userId }: { userId: string }) {
-    const { rows } = await this.db.query(sql`
-      SELECT posts.* FROM posts, posts_users
+      SELECT posts.* FROM posts
       WHERE posts.user_id = ${userId}
     `);
-    this._writePostToCache(rows[0]);
-    return rows;
+
+    return rows || null;
   }
 
-  async findPostLikedByUser(userId: string) {
-    const rows = await this.loaders.posts.load({ userId });
-    return rows[0] || null;
-  }
-  private async _findPostLikedByUser(userId: string) {
+  private async findPostsLikedByUser(userId: string) {
     const { rows } = await this.db.query(sql`
       SELECT posts.* FROM posts, posts_liked_users
       WHERE posts_liked_users.user_id = ${userId}
       AND posts.user_id = ${userId}
     `);
-    this._writePostToCache(rows[0]);
-    return rows;
-  }
 
-  async findPostById(postId: string) {
-    const rows = await this.loaders.posts.load({ postId });
-    return rows[0] || null;
-  }
-  private async _findPostById(postId: string) {
-    const { rows } = await this.db.query(sql`
-      SELECT * FROM posts WHERE id = ${postId}
-    `);
-    this._writePostToCache(rows[0]);
-    return rows;
+    return rows || null;
   }
 
   async addPost({
@@ -121,16 +70,16 @@ export class Posts {
   async removePost(postId: string) {
     try {
       await this.db.query('BEGIN');
-      const { rows } = await this.db.query(sql`
-        SELECT posts.* FROM posts, posts_liked_users
-        WHERE id = ${postId}
-        AND posts.id = posts_liked_users.post_id
-      `);
-      const post = rows[0];
-      if (!post) {
-        await this.db.query('ROLLBACK');
-        return null;
-      }
+      // const { rows } = await this.db.query(sql`
+      //   SELECT posts.* FROM posts, posts_liked_users
+      //   WHERE post_id = ${postId}
+      //   AND user.id = posts_liked_users.post_id
+      // `);
+      // const post = rows[0];
+      // if (!post) {
+      //   await this.db.query('ROLLBACK');
+      //   return null;
+      // }
       await this.db.query(sql`
         DELETE FROM posts WHERE posts.id = ${postId}
       `);
@@ -139,16 +88,6 @@ export class Posts {
     } catch (e) {
       await this.db.query('ROLLBACK');
       throw e;
-    }
-  }
-
-  private _readPostFromCache(postId: string) {
-    return this.postsCache.get(postId);
-  }
-
-  private _writePostToCache(post?: Post) {
-    if (post) {
-      this.postsCache.set(post.id, post);
     }
   }
 }
