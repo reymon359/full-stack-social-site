f496682a0ad4a91d75e1b2fec9de5945dd3a497a
reymon359 <ramonmorcillocascales@gmail.com>
2020-06-17 23:55:07 +0200

    test: add enables prints server error test

--- client/src/components/Auth/AuthForms/SignUpForm.test.tsx
+++ client/src/components/Auth/AuthForms/SignUpForm.test.tsx
@@ -19,7 +19,7 @@ describe('SignUpForm', () => {
 
   it('enables sign-up button when filled in', async () => {
     const history = createMemoryHistory();
-    const client = mockApolloClient();
+    const client = mockApolloClient([]);
 
     let getByTestId: any = null;
 
--- client/src/components/NewPost/NewPostForm.test.tsx
+++ client/src/components/NewPost/NewPostForm.test.tsx
@@ -9,7 +9,7 @@ import {
   waitFor,
 } from '@testing-library/react';
 import NewPostForm from './NewPostForm';
-import { AddChatDocument } from '../../graphql/types';
+import { AddPostDocument } from '../../graphql/types';
 import { mockApolloClient } from '../../test-helpers';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../../styles';
@@ -55,5 +55,71 @@ describe('NewPostForm', () => {
     await waitFor(() => expect(descriptionInput.value).toEqual('description'));
     await waitFor(() => expect(contentInput.value).toEqual('content'));
     await waitFor(() => expect(addPostButton.disabled).toEqual(false));
+  });
+
+  it('prints server error if input was wrong', async () => {
+    const history = createMemoryHistory();
+
+    const client = mockApolloClient([
+      {
+        request: {
+          query: AddPostDocument,
+          variables: {
+            title: 'title',
+            picture: 'picture',
+            description: 'description',
+            content: 'content',
+          },
+        },
+        get result() {
+          throw Error('add-post failed');
+        },
+      },
+    ]);
+
+    let getByTestId: any = null;
+
+    act(() => {
+      getByTestId = render(
+        <ThemeProvider theme={theme}>
+          <ApolloProvider client={client}>
+            <NewPostForm history={history} />
+          </ApolloProvider>
+        </ThemeProvider>
+      ).getByTestId;
+    });
+
+    const titleInput = await waitFor(() => getByTestId('title-input'));
+    const descriptionInput = await waitFor(() =>
+      getByTestId('description-input')
+    );
+    const pictureInput = await waitFor(() => getByTestId('picture-input'));
+    const contentInput = await waitFor(() => getByTestId('content-input'));
+
+    const addPostButton = await waitFor(
+      () => getByTestId('add-post-button') as HTMLButtonElement
+    );
+
+    act(() => {
+      fireEvent.change(titleInput, { target: { value: 'title' } });
+      fireEvent.change(descriptionInput, { target: { value: 'description' } });
+      fireEvent.change(pictureInput, { target: { value: 'picture' } });
+      fireEvent.change(contentInput, { target: { value: 'content' } });
+    });
+
+    await waitFor(() => expect(titleInput.value).toEqual('title'));
+    await waitFor(() => expect(descriptionInput.value).toEqual('description'));
+    await waitFor(() => expect(pictureInput.value).toEqual('picture'));
+    await waitFor(() => expect(contentInput.value).toEqual('content'));
+
+    act(() => {
+      fireEvent.click(addPostButton);
+    });
+
+    const errorMessage = await waitFor(() => getByTestId('message'));
+
+    await waitFor(() =>
+      expect(errorMessage.innerHTML).toContain('add-post failed')
+    );
   });
 });
