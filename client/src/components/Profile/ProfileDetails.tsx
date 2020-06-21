42dfc36a423efaceac09416a177ebfed401cc39e
reymon359 <ramonmorcillocascales@gmail.com>
2020-06-21 16:37:18 +0200

    refactor: update profile user picture size

--- client/src/components/Profile/ProfileDetails.tsx
+++ client/src/components/Profile/ProfileDetails.tsx
@@ -14,13 +14,14 @@ const ProfileDetailsContainer = styled.div`
 
 const UserPicture = styled.img`
   border-radius: 100%;
-  height: 20%;
-  width: 20%;
-  min-height: 100px;
-  min-width: 100px;
   max-height: 220px;
   max-width: 220px;
   border: 3px solid ${(props) => props.theme.colors.primaryDark};
+
+  ${(props) => props.theme.media.sm`
+    max-height: 100px;
+    max-width: 100px;
+ `}
 `;
 
 const UserName = styled.h1`
