a352a76cb4607b912f1cd8dc3f4444b6393d0035
reymon359 <ramonmorcillocascales@gmail.com>
2020-06-21 16:03:01 +0200

    feat: add tabs on profile

--- client/src/components/Profile/ProfileTabs.tsx
+++ client/src/components/Profile/ProfileTabs.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { History } from 'history';
-import { Navbar } from '../Navbar';
 import gql from 'graphql-tag';
 import * as fragments from '../../graphql/fragments';
-import { useGetUserPostsQuery, User } from '../../graphql/types';
-import ProfileDetails from './ProfileDetails';
-import { NotFoundContent } from '../NotFound';
+import { useGetUserPostsQuery } from '../../graphql/types';
 import PostsList from '../Shared/PostsList';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import styled from 'styled-components';
+import Toolbar from '@material-ui/core/Toolbar';
 
 // eslint-disable-next-line
 const getUserPostsQuery = gql`
@@ -27,6 +26,33 @@ const getUserPostsQuery = gql`
   ${fragments.user}
 `;
 
+const StyledTabs = styled(Tabs)`
+  width: 100%;
+`;
+
+const StyledTabList = styled(TabList)`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+`;
+
+const StyledTab = styled(Tab)`
+  margin: 0;
+  font-size: ${(props) => props.theme.fontSizes.medium};
+  font-weight: ${(props) => props.theme.fontWeights.bold};
+  cursor: pointer;
+  padding-bottom: 12px;
+
+  &:focus,
+  &:hover {
+    outline: none;
+    padding-bottom: 10px;
+    border-bottom: 2px solid ${(props) => props.theme.colors.primary};
+  }
+`;
+
+const StyledTabPanel = styled(TabPanel)``;
+
 interface ProfileTabsParams {
   userId: string;
 }
@@ -37,14 +63,25 @@ export const ProfileTabs: React.FC<ProfileTabsParams> = ({ userId }) => {
   });
   return (
     <>
-      <h2>User Posts</h2>
-      {loadingUserPosts ? (
-        <h1>Loading posts...</h1>
-      ) : data && data.userPosts ? (
-        <PostsList posts={data.userPosts} />
-      ) : (
-        <h1>No user posts found</h1>
-      )}
+      <StyledTabs>
+        <StyledTabList>
+          <StyledTab>Posts</StyledTab>
+          <StyledTab>Liked Posts</StyledTab>
+        </StyledTabList>
+
+        <StyledTabPanel>
+          {loadingUserPosts ? (
+            <h1>Loading posts...</h1>
+          ) : data && data.userPosts ? (
+            <PostsList posts={data.userPosts} />
+          ) : (
+            <h1>No user posts found</h1>
+          )}
+        </StyledTabPanel>
+        <StyledTabPanel>
+          <h2>Liked Posts content</h2>
+        </StyledTabPanel>
+      </StyledTabs>
     </>
   );
 };
